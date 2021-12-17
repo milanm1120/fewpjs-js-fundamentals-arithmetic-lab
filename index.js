540cdac36dcd71e226c789226a43d7de46e4db0c
Milan <milanm1120@gmail.com>
2021-12-17 14:08:29 -0500

    commit tree 7dc636df140b6d18ca1c9879b6f2f7fec9651375

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1= 2;
+let num2= 31;
+let num3= 19;
+let num4= 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 4, 6, 9, 14, 18, 20);
